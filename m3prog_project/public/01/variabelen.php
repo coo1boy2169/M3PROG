b65272a5776f1ed8f26200824fb2b4a05177910c
Coo1boy2169 <>
2024-02-29 14:27:04 +0100

    datatypes 02

---
+++ m3prog_project/public/01/variabelen.php
@@ -0,0 +1,22 @@
+<?php 
+$naam = 'olgun';
+$woon = 'Alkmaar';
+$leeftijd = 16;
+$temp= 10.3;
+ 
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php   echo "mijn naam is $naam en ik woon in een stad genaamd $woon en ik ben $leeftijd jaar oud en het is buiten $temp grade" ?>
+    
+</body>
+</html>
---
+++ m3prog_project/public/02/datatypes.php
@@ -0,0 +1,23 @@
+<?php     
+    $huisnummer = 175;
+    $geslaagt = true;
+    $nietgeslaagt = false; 
+    $prijs = 2.02;
+    $liter = 1 ;
+    $niks ="";
+
+
+    echo $nietgeslaagt;
+
+    echo $prijs;
+
+    echo $liter;
+
+    echo $niks ;
+
+    echo $huisnummer;
+    
+    echo $geslaagt;
+    
+
+?>
