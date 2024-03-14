02aef9e3f903d491edcd980c749c23c595879eae
Coo1boy2169 <>
2024-03-14 10:33:56 +0100

    04 les 2

--- m3prog_project/public/04/script.php
+++ m3prog_project/public/04/script.php
@@ -2,4 +2,18 @@
     include_once 'functions.php';
 
         echo keer();
+
+            
+        echo  berekenRitKosten(663, 15, 1.90);
+        echo '</br>';
+        echo  berekenRitKosten(1438 , 20 , 1.46);
+        
+        echo '</br>';
+        echo  berekenRitKosten(1991  , 18.47  , 2.23);
+
+        $afronding = berekenRitKosten(1991  , 18.47  , 2.23);
+        $afrond = number_format($afronding);
+        echo "</br> ";
+
+        echo $afrond;
 ?>
