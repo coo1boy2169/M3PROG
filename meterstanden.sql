5384768ad54ca7d2eb25b38304f6e0a2a15ffae9
Coo1boy2169 <>
2024-03-28 10:40:20 +0100

    06 opdracht 2

--- meterstanden.sql
+++ meterstanden.sql
@@ -48,3 +48,74 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Gegenereerd op: 28 mrt 2024 om 09:35
+-- Serverversie: 11.3.2-MariaDB-1:11.3.2+maria~ubu2204
+-- PHP-versie: 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m3prog_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `meterstanden`
+--
+
+CREATE TABLE `meterstanden` (
+  `id` int(11) NOT NULL,
+  `stand` int(11) NOT NULL,
+  `soort` varchar(16) NOT NULL,
+  `datum` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `meterstanden`
+--
+
+INSERT INTO `meterstanden` (`id`, `stand`, `soort`, `datum`) VALUES
+(1, 10000, 'electriciteit', '2023-12-01'),
+(2, 10500, 'electriciteit', '2023-12-02'),
+(3, 11000, 'electriciteit', '2023-12-03'),
+(4, 12500, 'electriciteit', '2023-12-04'),
+(5, 13500, 'electriciteit', '2023-12-05');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `meterstanden`
+--
+ALTER TABLE `meterstanden`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `meterstanden`
+--
+ALTER TABLE `meterstanden`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
